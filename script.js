f51cb27578229b4da0ec80f45d1215ef10013ae6
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-04-06 17:43:59 +0200

    Creazione prompt

--- script.js
+++ script.js
@@ -2,18 +2,24 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+// Creazione numeri
+
 let numbers = [];
 while (numbers.length < 5) {
   let r = Math.floor(Math.random() * 100) + 1;
   if (numbers.indexOf(r) === -1) numbers.push(r);
 }
 
+// Print numeri
+
 document.getElementById("random-numbers").innerHTML =
   "Memorizza i seguenti numeri entro 30 secondi: " + numbers;
 
 console.log(numbers);
 
-let timeLeft = 10;
+//Timer
+
+let timeLeft = 5;
 let quizTimer = setInterval(function () {
   if (timeLeft <= 0) {
     clearInterval(quizTimer);
@@ -25,3 +31,19 @@ let quizTimer = setInterval(function () {
   }
   timeLeft -= 1;
 }, 1000);
+
+// Push numeri
+
+let arr = []; // define our array
+
+for (let i = 0; i < 5; i++) {
+  // loop 10 times
+  arr.push(
+    prompt(
+      "Riscrivi i numeri appena visualizzati uno alla volta! Sei al numero: " +
+        (i + 1)
+    )
+  ); // push the value into the array
+}
+
+console.log("Full array: " + arr.join(", "));
